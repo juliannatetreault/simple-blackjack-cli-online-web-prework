15b05478b23cc209f476ead4e579a0d839312177
Julianna Tetreault <juliannatetreault@icloud.com>
2018-09-10 06:29:14 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -53,9 +53,8 @@ end
 #####################################################
 
 def runner
-  card_total = 0
   welcome
-  initial_round
+  card_total = initial_round
   hit?(card_total)
   display_card_total(card_total)
   until card_total > 21
