a1e0693b3da4b8fe28a831318fc81f9b39123f09
Julianna Tetreault <juliannatetreault@icloud.com>
2018-09-10 06:25:50 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -57,10 +57,10 @@ def runner
   welcome
   initial_round
   hit?
-  display_card_total
+  display_card_total(card_total)
   until card_total > 21
   hit?
-  display_card_total
+  display_card_total(card_total)
 end
   end_game(card_total)
 end
